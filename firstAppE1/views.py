d69c2860aecf3dd49aca6c9b25f945e166912100
josed <jose.diaz4@cloud.uautonoma.cl>
2022-10-08 22:37:00 -0300

    Se añadieron datos en el diccionario de datos y +

--- firstAppE1/views.py
+++ firstAppE1/views.py
@@ -4,15 +4,15 @@ from importlib.machinery import SourceFileLoader
 # Create your views here.
 
 def index (request):
-        data = {
+    data = {
         "nombre" : 'José',
         "apellido" : 'Díaz',
         "profesion" : 'Casi ingeniero, ojalá',
         "edad" : '22',
         "ciudad" : 'Talca',
-        "perfil" : '/static/img/vikings.svg'
+        "perfil" : '/static/img/vikings.svg'        
     }
-        return render (request, 'index.html',data)
+    return render (request, 'index.html',data)
 
 
 def renderproyecto1(request):
@@ -22,7 +22,12 @@ def renderproyecto1(request):
         "cliente" : 'publico en general',
         "año" : '2020',
         "imagencita" : '/static/img/imgproyecto1.png',
-        "enlace" : 'http://www.rleonardi.com'
+        "enlace" : 'http://www.rleonardi.com',
+        "desarrolladores" : '100',
+        "norma" : 'norma ISO/IEC 33000',
+        "calificacion" : '10 de 10',
+        "siguiente" : '/proyectos2'
+
     }
     return render (request, 'proyectos.html', data) 
 
@@ -30,12 +35,16 @@ def renderproyecto1(request):
 def renderproyecto2(request):
     data = {
         "nombreproyecto" : 'Yul Moreau',
-        "detalle" : 'segundo detalle',
-        "cliente" : 'cliente recibido dos',
-        "año" : '2004',
+        "detalle" : 'Se inspira en los años 80 y dedica su portafolio a esta época. Con un diseño de página única con videos incrustados.',
+        "cliente" : 'cliente privado',
+        "año" : '2012',
         "imagencita" : '/static/img/imgproyecto2.png',
-        "enlace" : 'https://y78.fr'
-
+        "enlace" : 'https://y78.fr',
+        "desarrolladores" : '75',
+        "norma" : 'norma ISO 9001',
+        "calificacion" : '8 de 10',
+        "siguiente" : '/proyectos3'
+ 
     }
     return render (request, 'proyectos.html', data) 
 
@@ -43,10 +52,14 @@ def renderproyecto2(request):
 def renderproyecto3(request):
     data = {
         "nombreproyecto" : 'Daniel Spatzek',
-        "detalle" : 'Detalle del proyecto3',
-        "cliente" : 'cliente recibido3',
-        "año" : '2001',
+        "detalle" : 'utiliza un montón de trucos y animaciones de CSS que le dan vida al portafolio y crean una experiencia verdaderamente dinámica.',
+        "cliente" : 'empresa de desarrollo',
+        "año" : '2016',
         "imagencita" : '/static/img/imgproyecto3.png',
-        "enlace" : 'https://www.danielspatzek.com/home/'
+        "enlace" : 'https://www.danielspatzek.com/home/',
+        "desarrolladores" : '50',
+        "norma" : 'norma ISO/IEC 14598',
+        "calificacion" : '7 de 10',
+        "siguiente" : '/proyectos'
     }
     return render (request, 'proyectos.html', data) 
